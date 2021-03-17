6e4a95df9a472e1154ecddcda95891733c218e7b
reginaldo <reginaldobrain@gmail.com>
2021-03-17 09:56:25 -0300

    sbo 170321 9:56

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Rout::get()
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
